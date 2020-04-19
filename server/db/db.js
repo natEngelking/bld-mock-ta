7c77dcc796a64a68e0665e5d5b9798140151d034
natEngelking <natalie.mixdorf@gmail.com>
2020-04-19 15:16:10 -0600

    database check

--- server/db/db.js
+++ server/db/db.js
@@ -1,1 +1,24 @@
 const mongoose = require('mongoose');
+const dbPath = 'mongodb://localhost/mocktadb';
+
+mongoose.connect(dbPath);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+
+const Schema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  description: String
+});
+
+let Cow = mongoose.model('Cow', Schema);
+
+//make dymanic
+let buttercup = new Cow({
+  id: 1,
+  name: 'Buttercup',
+  description: 'a herbaceous plant with bright yellow cup-shaped flowers, common in grassland and as a garden weed. All kinds are poisonous and generally avoided by livestock.'
+})
+buttercup.save();
+
+module.exports = mongoose;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const cors = require('cors')
+const db = require('./db/db.js')
 
 const app = express();
 const port = 8080;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+//requesting cow info
+app.get('/api/cow', async (req, res) => {
+try {
+  let result = await Cow.find();
+  res.send(result);
+} catch (err) {
+  res.status(404).send(err);
+}
+})
+
+//need more cow(bell)
+app.post('/api/cow', aysnc (req, res) => {
+  try {
+    //???
+   let result = Cow.create({
+      id: req.body.id,
+      name: req.body.name,
+      description: req.body.description
+    });
+    let result = await cow.save();
+    response.send(result);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
